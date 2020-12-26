a4b6d18c71c6ff057bb6646f46275840b11d4b67
daniel.ofir <daniel.ofir@imperva.com>
2020-12-26 17:29:59 +0200

    maybe it's fixed

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,17 +1,17 @@
-docker build -t danielofir/multi-client:latest -t danielofir/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t danielofir/multi-server:latest -t danielofir/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t danielofir/multi-worker:latest -t danielofir/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t danielofir/complex-client:latest -t danielofir/complex-client:$SHA -f ./client/Dockerfile ./client
+docker build -t danielofir/complex-server:latest -t danielofir/complex-server:$SHA -f ./server/Dockerfile ./server
+docker build -t danielofir/complex-worker:latest -t danielofir/complex-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push danielofir/multi-client:latest
-docker push danielofir/multi-server:latest
-docker push danielofir/multi-worker:latest
+docker push danielofir/complex-client:latest
+docker push danielofir/complex-server:latest
+docker push danielofir/complex-worker:latest
 
-docker push danielofir/multi-client:$SHA
-docker push danielofir/multi-server:$SHA
-docker push danielofir/multi-worker:$SHA
+docker push danielofir/complex-client:$SHA
+docker push danielofir/complex-server:$SHA
+docker push danielofir/complex-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=danielofir/multi-client:$SHA
-kubectl set image deployments/server-deployment server=danielofir/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=danielofir/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=danielofir/complex-client:$SHA
+kubectl set image deployments/server-deployment server=danielofir/complex-server:$SHA
+kubectl set image deployments/worker-deployment worker=danielofir/complex-worker:$SHA
 
