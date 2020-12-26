4c96bf5dd49315773ad55d8962f0a1a0f2271353
daniel.ofir <daniel.ofir@imperva.com>
2020-12-26 17:50:35 +0200

    maybe it's fixed

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -2,13 +2,13 @@ docker build -t danielofir/complex-client:latest -t danielofir/complex-client:$S
 docker build -t danielofir/complex-server:latest -t danielofir/complex-server:$SHA -f ./server/Dockerfile ./server
 docker build -t danielofir/complex-worker:latest -t danielofir/complex-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push danielofir/complex-client:latest
-docker push danielofir/complex-server:latest
-docker push danielofir/complex-worker:latest
+# docker push danielofir/complex-client:latest
+# docker push danielofir/complex-server:latest
+# docker push danielofir/complex-worker:latest
 
-docker push danielofir/complex-client:$SHA
-docker push danielofir/complex-server:$SHA
-docker push danielofir/complex-worker:$SHA
+# docker push danielofir/complex-client:$SHA
+# docker push danielofir/complex-server:$SHA
+# docker push danielofir/complex-worker:$SHA
 
 # kubectl apply -f k8s
 # kubectl set image deployments/client-deployment client=danielofir/complex-client:$SHA
